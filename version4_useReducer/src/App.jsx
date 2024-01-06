ad8a857a52f53d7a9902811b9062cd55f93e655c
NSD <niloysd7@gmail.com>
2024-01-06 22:14:30 +0530

    Cleaning the App Component

--- version4_useReducer/src/App.jsx
+++ version4_useReducer/src/App.jsx
@@ -2,72 +2,21 @@ import AppName from "./components/AppName";
 import TodoItemsContainer from "./components/TodoItemsContainer";
 import WelcomeMsg from "./components/WelcomeMsg";
 
-import { useReducer } from "react";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 //useReducer is a pure function. Pure fuctions only deal with their own arguments. They don't touch anything outside the function.
 
 // A reducer functions takes current state and an action object and returns the new state.
-const todoListReducer = (currentTodoList, action) => {
-  let newTodoList = currentTodoList;
-  if (action.type === "NEW_ITEM") {
-    const newId = currentTodoList.length
-      ? currentTodoList[currentTodoList.length - 1].id + 1
-      : 0;
-    newTodoList = [
-      ...currentTodoList,
-      { id: newId, name: action.payload.newTodo, date: action.payload.newDate },
-    ];
-  } else if (action.type === "DELETE_ITEM") {
-    newTodoList = currentTodoList.filter(
-      (item) => item.id !== action.payload.id
-    );
-  }
-
-  localStorage.setItem("todoArray", JSON.stringify(newTodoList));
-  return newTodoList;
-};
 
 function App() {
-  const todoArray = JSON.parse(localStorage.getItem("todoArray")) || [];
-
-  const [todoList, dispatchTodoList] = useReducer(todoListReducer, todoArray);
-
-  // useEffect(
-  //   () => localStorage.setItem("todoArray", JSON.stringify(todoList)),
-  //   [todoList]
-  // );
-
-  const addNewItem = (newTodo, newDate) => {
-    const addNewItemAction = {
-      type: "NEW_ITEM",
-      payload: {
-        newTodo,
-        newDate,
-      },
-    };
-    dispatchTodoList(addNewItemAction);
-  };
-
-  const deleteItem = (id) => {
-    const deleteItemAction = {
-      type: "DELETE_ITEM",
-      payload: {
-        id,
-      },
-    };
-    dispatchTodoList(deleteItemAction);
-  };
-
   return (
-    <TodoItemsContext.Provider value={{ todoList, addNewItem, deleteItem }}>
-      {/*Shorthand for: todoList: todoList, addNewItem: addNewItem, deleteItem: deleteItem*/}
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName></AppName>
         <TodoItemsContainer></TodoItemsContainer>
         <WelcomeMsg></WelcomeMsg>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
--- version4_useReducer/src/store/todo-items-store.jsx
+++ version4_useReducer/src/store/todo-items-store.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const TodoItemsContext = createContext(
   {
@@ -7,3 +7,63 @@ export const TodoItemsContext = createContext(
     deleteItem: () => {},
   } // Just so that the values show up is vsCode autocomplete.
 );
+
+const todoListReducer = (currentTodoList, action) => {
+  let newTodoList = currentTodoList;
+  if (action.type === "NEW_ITEM") {
+    const newId = currentTodoList.length
+      ? currentTodoList[currentTodoList.length - 1].id + 1
+      : 0;
+    newTodoList = [
+      ...currentTodoList,
+      {
+        id: newId,
+        name: action.payload.newTodo,
+        date: action.payload.newDate,
+      },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoList = currentTodoList.filter(
+      (item) => item.id !== action.payload.id
+    );
+  }
+
+  localStorage.setItem("todoArray", JSON.stringify(newTodoList));
+  return newTodoList;
+};
+
+const TodoItemsContextProvider = ({ children }) => {
+  const todoArray = JSON.parse(localStorage.getItem("todoArray")) || [];
+
+  const [todoList, dispatchTodoList] = useReducer(todoListReducer, todoArray);
+
+  const addNewItem = (newTodo, newDate) => {
+    const addNewItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        newTodo,
+        newDate,
+      },
+    };
+    dispatchTodoList(addNewItemAction);
+  };
+
+  const deleteItem = (id) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        id,
+      },
+    };
+    dispatchTodoList(deleteItemAction);
+  };
+
+  return (
+    <TodoItemsContext.Provider value={{ todoList, addNewItem, deleteItem }}>
+      {/*Shorthand for: todoList: todoList, addNewItem: addNewItem, deleteItem: deleteItem*/}
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
